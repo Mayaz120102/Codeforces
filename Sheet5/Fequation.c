0ca89f0fedb4fb6aabb92c2967ec19be55937ff3
Mayaz120102 <abrarmayaz2002@gmail.com>
2024-08-29 20:25:19 +0600

    equation

--- Sheet5/Fequation.c
+++ Sheet5/Fequation.c
@@ -1,31 +1,28 @@
 #include<stdio.h>
 //
 // Function to compute power of X^Y without using built-in functions
-int power(int x, int y){
- int result = 1;
- for (int i = 0; i < y; i++)
+long long power(long long x, long long y){
+ long long result = 1;
+ for (int i = 1; i<= y; i++)
  {
     result *= x;
  }
  return result;
 }
 // Function to calculate the sum S
-int calcSum(int x, int n){
-    int S = 0;
-    S += power(x,0)-1; // Initial term (X^0 - 1)
 
-    for (int i = 2; i <= n; i+=2)
-    {
-      S += power(x,i);
-    }
-   return S; 
-}
 int main(){
-    int X,N;
-    scanf("%d %d", &X,&N);
+    long long  x,n;
+    scanf("%lld %lld", &x,&n);
     
-    int result = calcSum(X,N);
-    printf("%d\n",result);
+    long long s =0;
+    for (int  i = 2; i <= n; i+=2)
+    {
+        s += power(x, i);
+    }
+    
+   
+    printf("%d\n",s);
     
     return 0;
     
