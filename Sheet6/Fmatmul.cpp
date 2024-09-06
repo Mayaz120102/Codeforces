ff885e0048be7fb1f2613b66e9d5804a086b5836
Mayaz120102 <abrarmayaz2002@gmail.com>
2024-09-06 15:26:07 +0600

    updated

--- Sheet6/Fmatmul.cpp
+++ Sheet6/Fmatmul.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+
 int main()
 {
     int r1,c1;
@@ -14,31 +15,27 @@ int main()
     int r2,c2;
     cin>>r2>>c2;
 
-   int  b[100][100],m[100][100];
+    int  b[100][100];
 
-    
-    
      for(int i=0;i<r2;i++){
         for(int j=0;j<c2;j++){
             cin>>b[i][j];
         }
     }
    
-   int sum =0;
-   for(int i=0; i<r1;i++){
-    for(int j=0; j<c1;j++){
-        for(int k=0;k<c1;k++){
-            sum = sum +a[i][k]*b[k][j];
-        }
+   int m[100][100]={0};
 
-        m[i][j] =sum;
-        sum =0;
+   for(int i=0; i<r1;i++){
+    for(int j=0; j<c2;j++){
+        for(int k=0;k<c1;k++){
+            m[i][j]+= a[i][k]*b[k][j];
+        }
     }
-   }
+ }
 
    for(int i =0;i<r1;i++){
     for(int j=0;j<c2;j++){
-        cout<<m[i][j]<<" ";    
+        cout << m[i][j] << ' ';    
    }
    cout<<endl;
    }
